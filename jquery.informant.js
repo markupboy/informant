605f050f8e46e37ecb4c80b5123878c56176cd4a
Blake Walters <blake@markupboy.com>
2015-06-22 16:43:03 -0600

    updated plugin to add public methods for disabling

--- jquery.informant.js
+++ jquery.informant.js
@@ -1,32 +1,53 @@
-(function($) {
+(function(window, $) {
 
-	$.fn.informant = function(options) {
-		options = $.extend($.fn.informant.defaults, options);
-		
-		var inform = false;
-		
-		$(window).on('beforeunload', function() {
-			if(inform) {
-				return options.message;
-			}
-		});
-		
-		return this.each(function() {
-			var form = $(this);
-			var before = form.serialize();
-			
-			form.change(function() {
-				var after = form.serialize();
-				inform = before != after;
-			}).submit(function() {
-				inform = false;
-				before = form.serialize();
-			});
-		});
-	};
-	
-	$.fn.informant.defaults = {
-		message: 'All unsaved changes will be lost.'
-	};
+  var Informant = function(scope, options) {
+    this.options = $.extend({}, this.defaults, options)
+    this.scope = $(scope);
+    this.inform = false;
+    this.init();
+  };
 
-})(jQuery);
+  Informant.prototype = {
+    init: function() {
+      this.initialState = this.captureState();
+      this.on();
+    },
+
+    on: function() {
+      $(window).on('beforeunload.informant', function() {
+        if(this.inform) {
+          return this.options.message;
+        }
+      }.bind(this));
+
+      this.scope.on('change.informant', function() {
+        this.inform = this.initialState != this.captureState();
+      }.bind(this));
+
+      this.scope.on('submit.informant', function() {
+        this.inform = false;
+        this.initialState = this.captureState;
+      }.bind(this));
+    },
+
+    off: function() {
+      $(window).off('beforeunload.informant');
+      this.scope.off('submit.informant change.informant');
+    },
+
+    captureState: function() {
+      return this.scope.serialize();
+    },
+
+    defaults: {
+      message: 'All unsaved changes will be lost.'
+    }
+  }
+
+  $.fn.informant = function(options) {
+    return $(this).each(function() {
+      $(this).data('informant', new Informant(this, options));
+    })
+  }
+
+})(window, jQuery)
